055e09d92cfc823839642f80aee0cf447ec316e6
XFillD <haubafilip@gmail.com>
2023-11-05 18:32:33 +0100

    headings uprava

--- components/Layout/SectionWrapper.tsx
+++ components/Layout/SectionWrapper.tsx
@@ -2,19 +2,36 @@ import { cn } from "@/lib/classnames";
 import { FC, ReactNode } from "react";
 
 interface SectionWrapperProps {
-    children: ReactNode;
-    title: string;
-    id: string;
-    className?: string;
+  children: ReactNode;
+  title: string;
+  id: string;
+  className?: string;
 }
 
-const SectionWrapper: FC<SectionWrapperProps> = ({ children,title, id, className }) => {
-    return (
-        <section id={id} className={cn("w-full relative bg-white overflow-hidden p-2 space-y-2", className)}>
-            <div className="bg-black w-full max-w-7xl mx-auto p-2"><h3 className="text-4xl md:text-5xl font-bold text-mainColor">{title}</h3></div>
-            <div className="w-full max-w-7xl mx-auto flex flex-colp-2 pb-16 md:pb-12">{children}</div>
-        </section>
-    );
+const SectionWrapper: FC<SectionWrapperProps> = ({
+  children,
+  title,
+  id,
+  className,
+}) => {
+  return (
+    <section
+      id={id}
+      className={cn(
+        "w-full relative bg-white overflow-hidden p-2 space-y-2",
+        className
+      )}
+    >
+      <div className="bg-black w-full max-w-7xl mx-auto p-2 rounded-lg bg-gradient-to-r from-gray-500 via-black to-yellow-200">
+        <h3 className="text-4xl md:text-5xl font-bold text-mainColor text-center">
+          {title}
+        </h3>
+      </div>
+      <div className="w-full max-w-7xl mx-auto flex flex-colp-2 pb-16 md:pb-12">
+        {children}
+      </div>
+    </section>
+  );
 };
 
 export default SectionWrapper;
